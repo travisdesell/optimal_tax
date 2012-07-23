c29c8ea06e52dce3f54c5c74f994606c712303ec
Harry Tsang <harrytsang@gmail.com>
2012-07-23 12:25:48 -0500

    Updated elasticities.cxx to include a counter variable for parameter_sweep

--- elasticities.cxx
+++ elasticities.cxx
@@ -67,9 +67,10 @@ double penalty_out_inclusive(double min, double max, double value) {
     return 0;
 }
 
-double objective_function(const vector<double> &A, uint64_t iteration) {
+double objective_function(const vector<double> &A) {
     double f = 0;
     uint32_t success_count = 0;
+	uint64_t iteration_count = 0;
 
 	double A_1 = A[0];
 	double A_2 = A[1];
@@ -129,75 +130,8 @@ double objective_function(const vector<double> &A, uint64_t iteration) {
 
 
     if (success_count >= 14 && f > -0.01) {
-        (*output_csv) << iteration << ", " << A[0] << ", " << A[1] << ", " << A[2] << ", " << A[3] << ", " << A[4] << ", " << A[5] << ", " << success_count << ", " << f << ", " << mux_v << ", " << muy_v << ", " << mul_v << ", " << elastxp << ", " << elastxq << ", " << elastxw << ", " << elastyp << ", " << elastyq << ", " << elastyw << ", " << elastlp << ", " << elastlq << ", " << elastlw << ", " << etax << ", " << etay << ", " << etal << ", " << minor2_v << ", " << minor3_v << ", " << minor4_v << endl;
-    }
-
-    return f;
-}
-
-double objective_function2(const vector<double> &A) {
-    double f = 0;
-    uint32_t success_count = 0;
-
-	double A_1 = A[0];
-	double A_2 = A[1];
-	double A_3 = A[2];
-	double B_1 = A[3];
-	double B_2 = A[4];
-	double B_3 = A[5];
-
-    /* mux_v > 0 */
-    /* muy_v > 0 */
-    /* mul_v > 0 */
-    double mux_v = mux(cleangood, dirtygood, leisuregood, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c);
-    double muy_v = muy(cleangood, dirtygood, leisuregood, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c);
-    double mul_v = mul(cleangood, dirtygood, leisuregood, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c);
-
-     if (mux_v > 0) success_count++; f -= penalty_leq(0, mux_v);
-     if (muy_v > 0) success_count++; f -= penalty_leq(0, muy_v);
-     if (mul_v > 0) success_count++; f -= penalty_leq(0, mul_v);
-
-    /* Elasticity Criteria: */
-    double elastxp = dxdp(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( cleanprice / cleangood );        /* -1 < elastxp < 0; */
-    double elastxq = dxdq(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( dirtyprice / cleangood );        /* abs val ( elastxq ) < 1; */
-    double elastxw = dxdw(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( netwage / cleangood);            /* abs val ( elastxw ) < 1; */
-
-     if (-1.25 < elastxp && elastxp < -0.50) success_count++; f -= penalty_out_inclusive(-1.25, -0.50, elastxp);
-     if (-1.00 < elastxq && elastxq < 1.00) success_count++; f -= penalty_out_inclusive(-1.00, 1.00, elastxq);
-     if (-1.25 < elastxw && elastxw < 1.25) success_count++; f -= penalty_out_inclusive(-1.25, 1.25, elastxw);
-
-    double elastyp = dydp(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( cleanprice / dirtygood );        /* abs val ( elastyp ) < 1; */
-    double elastyq = dydq(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( dirtyprice / dirtygood );        /* -1 < elastyq < 0; */
-    double elastyw = dydw(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( netwage / dirtygood );           /* abs val ( elastyw ) < 1; */
-
-     if (-1.00 < elastyp && elastyp < 1.00) success_count++; f -= penalty_out_inclusive(-1.00, 1.00, elastyp);
-     if (-1.00 < elastyq && elastyq < -0.20) success_count++; f -= penalty_out_inclusive(-1.00, -0.20, elastyq);
-     if (-1.25 < elastyw && elastyw < 1.25) success_count++; f -= penalty_out_inclusive(-1.25, 1.25, elastyw);
-
-    double elastlp = dldp(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( cleanprice / leisuregood );      /* abs val ( elastlp ) < 1; */
-    double elastlq = dldq(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( dirtyprice / leisuregood );      /* abs val ( elastlq ) < 1; */
-    double elastlw = dldw(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( netwage / leisuregood );         /* abs val ( elastlw ) < 1; */
-
-     if (-0.50 < elastlp && elastlp < 0.50) success_count++; f -= penalty_out_inclusive(-0.50, 0.50, elastlp);
-     if (-0.50 < elastlq && elastlq < 0.50) success_count++; f -= penalty_out_inclusive(-0.50, 0.50, elastlq);
-     if (-0.25 < elastlw && elastlw < 0.25) success_count++; f -= penalty_out_inclusive(-0.25, 0.25, elastlw);
-
-    double etax = dxdI(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( total_income / cleangood );       /* 0 < etax > 2; */
-    double etay = dydI(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( total_income / dirtygood );       /* 0 < etay > 2; */
-    double etal = dldI(cleanprice, dirtyprice, netwage, time_endowment, virtual_inc, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c) * ( total_income / leisuregood );     /* 0 < etal > 2; */
-
-     if (0.50 < etax && etax < 1.75) success_count++; f -= penalty_out_inclusive(0.50, 1.75, etax);
-     if (0.20 < etay && etay < 1.00) success_count++; f -= penalty_out_inclusive(0.20, 1.00, etay);
-     if (0.20 < etal && etal < 1.25) success_count++; f -= penalty_out_inclusive(0.20, 1.25, etal);
-
-    /* report minor 2, minor 3, minor 4 */
-    double minor2_v = minor2(cleangood, dirtygood, leisuregood, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c);
-    double minor3_v = minor3(cleangood, dirtygood, leisuregood, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c);
-    double minor4_v = minor4(cleangood, dirtygood, leisuregood, A_1, A_2, A_3, B_1, B_2, B_3, a, b, c);
-
-
-    if (success_count >= 14 && f > -0.01) {
-        (*output_csv) << A[0] << ", " << A[1] << ", " << A[2] << ", " << A[3] << ", " << A[4] << ", " << A[5] << ", " << success_count << ", " << f << ", " << mux_v << ", " << muy_v << ", " << mul_v << ", " << elastxp << ", " << elastxq << ", " << elastxw << ", " << elastyp << ", " << elastyq << ", " << elastyw << ", " << elastlp << ", " << elastlq << ", " << elastlw << ", " << etax << ", " << etay << ", " << etal << ", " << minor2_v << ", " << minor3_v << ", " << minor4_v << endl;
+		iteration_count++;
+        (*output_csv) << iteration_count << ", " << A[0] << ", " << A[1] << ", " << A[2] << ", " << A[3] << ", " << A[4] << ", " << A[5] << ", " << success_count << ", " << f << ", " << mux_v << ", " << muy_v << ", " << mul_v << ", " << elastxp << ", " << elastxq << ", " << elastxw << ", " << elastyp << ", " << elastyq << ", " << elastyw << ", " << elastlp << ", " << elastlq << ", " << elastlw << ", " << etax << ", " << etay << ", " << etal << ", " << minor2_v << ", " << minor3_v << ", " << minor4_v << endl;
     }
 
     return f;
@@ -241,20 +175,20 @@ int main(int number_arguments, char **argv) {
 
     if (search_type.compare("ps") == 0) {
         ParticleSwarm ps(min_bound, max_bound, arguments);
-        ps.iterate(objective_function2);
+        ps.iterate(objective_function);
 
     } else if (search_type.compare("de") == 0) {
         DifferentialEvolution de(min_bound, max_bound, arguments);
-        de.iterate(objective_function2);
+        de.iterate(objective_function);
 
     } else if (search_type.compare("sweep") == 0) {
         vector<double> step_size(6, 0);
-        step_size[0] = 0.10;
-        step_size[1] = 0.10;
-        step_size[2] = 0.10;
-		step_size[3] = 0.10;
-		step_size[4] = 0.10;
-		step_size[5] = 0.10;
+        step_size[0] = 1.00;
+        step_size[1] = 1.00;
+        step_size[2] = 1.00;
+		step_size[3] = 1.00;
+		step_size[4] = 1.00;
+		step_size[5] = 1.00;
 
         parameter_sweep(min_bound, max_bound, step_size, objective_function);
 
@@ -279,11 +213,11 @@ int main(int number_arguments, char **argv) {
         step_size[2] = 0.005;
 
         if (search_type.compare("snm") == 0) {
-            synchronous_newton_method(arguments, objective_function2, starting_point, step_size);
+            synchronous_newton_method(arguments, objective_function, starting_point, step_size);
         } else if (search_type.compare("gd") == 0) {
-            synchronous_gradient_descent(arguments, objective_function2, starting_point, step_size);
+            synchronous_gradient_descent(arguments, objective_function, starting_point, step_size);
         } else if (search_type.compare("cgd") == 0) {
-            synchronous_conjugate_gradient_descent(arguments, objective_function2, starting_point, step_size);
+            synchronous_conjugate_gradient_descent(arguments, objective_function, starting_point, step_size);
         }
 
     } else {
